04e11e9336f92d3ae07df60371cdb5d4a640d9fe
timothyAgevi <timothyagevi@gmail.com>
2022-02-18 00:50:03 +0300

    delete static state from shoppingList component

--- client/src/actions/itemsActions.js
+++ client/src/actions/itemsActions.js
@@ -1,1 +1,9 @@
+import res from 'express/lib/response';
 import {GET_ITEMS,ADD_ITEMS,DELETE_ITEM}from '../actions/types';
+
+export const getITems = () =>{
+    return {
+        type:GET_ITEMS,
+        payload:res.
+    }
+}
