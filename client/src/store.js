087ee5bb87bf80c1efcfa0508e5e75d63d8145cf
timothyAgevi <timothyagevi@gmail.com>
2022-02-17 21:16:39 +0300

    add redux store

--- client/src/store.js
+++ client/src/store.js
@@ -6,3 +6,9 @@ const initialState={};
 
 //middleware
 const middleware= [thunk]
+
+//store
+const store = createStore(rootReducer,initialState,compose( 
+    applyMiddleware(...middleware),
+    window._REDUX_DEVTOOLS_EXTENSION_ && window._REDUX_DEVTOOLS_EXTENSION_()
+))
