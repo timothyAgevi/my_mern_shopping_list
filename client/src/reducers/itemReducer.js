83b357ce8d81fb6e7c7310d41b1e8bc26f4acd6b
timothyAgevi <timothyagevi@gmail.com>
2022-02-18 00:12:50 +0300

    update itemReducer.js

---
+++ client/src/reducers/itemReducer.js
@@ -0,0 +1,10 @@
+import uuid from 'uuid';
+
+const initialState= {
+    items:[
+        { id :uuid(),name:'Eggs'},
+        { id :uuid(),name:'Milk'},
+        { id :uuid(),name:'Steak'},
+        { id :uuid(),name:'Water'}
+    ]
+}
