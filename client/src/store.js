237f0890f29e291bf96b1d61cc19b9fa8028cdc1
timothyAgevi <timothyagevi@gmail.com>
2022-02-17 21:18:17 +0300

    export redux store

--- client/src/store.js
+++ client/src/store.js
@@ -11,4 +11,6 @@ const middleware= [thunk]
 const store = createStore(rootReducer,initialState,compose( 
     applyMiddleware(...middleware),
     window._REDUX_DEVTOOLS_EXTENSION_ && window._REDUX_DEVTOOLS_EXTENSION_()
-))
+));
+
+export default store;
