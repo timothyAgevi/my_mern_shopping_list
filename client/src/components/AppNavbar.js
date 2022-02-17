014dea8a23bd5d609ffa38ab3b51c3ae5b7e7924
timothyAgevi <timothyagevi@gmail.com>
2022-02-17 16:31:58 +0300

    add  navbar

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -17,9 +17,26 @@ import{
      toggle= ()=>{
          this.setState({
              isOpen:!this.state.isOpen
-         }) 
+         }) ;
      }
 
+     render(){
+         <div>
+             <Navbar color="dark" dark expand ="sm" className="mb-5">
+                 <Container>
+                     <NavbarBrand href = "/">ShoppingList</NavbarBrand>
+                     <NavbarToggler onclick= {this.togggle}/>
+                     <Collapse isOpen ={this.state.isOpen}navbar>
+                     <Nav className='ml-auto' navbar>
+                         <NavItem>
+                             <NavLink href="https://github.com/timothyAgevi">Github</NavLink>
+                         </NavItem>
+                     </Nav>
+                     </Collapse>
+                 </Container>
+             </Navbar>
+         </div>
+     }
  }
      
          
